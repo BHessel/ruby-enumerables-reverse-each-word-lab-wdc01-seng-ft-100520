5ea3bcc96698196acce2c6a6b0afd5aab269fd9d
Benjamin Hessel <hesselfitness@gmail.com>
2020-09-17 23:02:52 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,17 +1,4 @@
-require 'pry'
-
-sentence1 = %w(Hello there, and how are you?)
-def reverse_each_word (sentence1)
-  final = []
-  result = sentence1.reverse
-  final << result.pop
+def reverse_each_word (string)
+  string.split.collect {|i| i.reverse}
+ 
 end
-
-
-#turn string into an array of strings with %w, assign to variable
-#create method, set variable as paramenter 
-#Currently it's reversing the whole sentence
-#How can I get one word to reverse, then the next, etc.?
-
-
-#getting an error b/c quotes are around each string in the array, use .join("") @ end
